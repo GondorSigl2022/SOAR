2ef027874df6661287b392bd3cf4e70e3a68d331
Thomas Vaton <35228405+Thomasvtn@users.noreply.github.com>
2021-12-14 19:49:08 +0100

    Update bdd-instances.tf

--- plateform2/infrastructure/bdd-instances.tf
+++ plateform2/infrastructure/bdd-instances.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "bdd-master" {
 
+  # AMI dispo sur la région de PARIS => changer par une AMI  de chez nous "us-east-1/2"
   ami           = "ami-0d1bf5b68307103c2"
   instance_type = "t2.micro"
 
@@ -30,6 +31,7 @@ resource "aws_instance" "bdd-master" {
 
 resource "aws_instance" "bdd-slave" {
 
+  # AMI dispo sur la région de PARIS => changer par une AMI  de chez nous "us-east-1/2"
   ami           = "ami-0d1bf5b68307103c2"
   instance_type = "t2.micro"
 

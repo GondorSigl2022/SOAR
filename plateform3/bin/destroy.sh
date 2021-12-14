f429bb248e57a74623294c3e75207a6adf2f93e8
Thomasvtn <vaton-thomas@outlook.fr>
2021-12-14 15:43:03 +0100

    plat3: update destroy script

--- plateform3/bin/destroy.sh
+++ plateform3/bin/destroy.sh
@@ -2,9 +2,12 @@
 
 cd infrastructure
 
+# Define the project name from terraform variables
+PROJECT_NAME="$(cat "variables.tf" | grep 'project_name' -A 2 | grep 'default' | cut -d '=' -f 2 | cut -d '"' -f 2)"
+
 # Delete s3 content
-aws s3 rm --recursive s3://mk-rtla-backend
-aws s3 rm --recursive s3://mk-rtla-frontend
+aws s3 rm --recursive "s3://$PROJECT_NAME-backend"
+aws s3 rm --recursive "s3://$PROJECT_NAME-frontend"
 
 # Delete terraform architecture
 terraform destroy -auto-approve
